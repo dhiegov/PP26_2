2ce1f47a4c16bcb945302bd2e1aaab91579eadf1
Dhiego da Costa <dhiegov@protonmail.com>
2026-09-12 00:21:14 -0300

    Implementar Adapter que implementa a interface da Calculadora perdida herdando da calculadora achada na internet

---
+++ adapter/Calculadora_Adapter.java
@@ -0,0 +1,35 @@
+
+import Calculadora_202321250008;
+
+public class Calculadora_Adapter
+extends Calculadora_202321250008
+implements ICalculadora_Dhiego {
+
+    @Override
+    public int soma(int a, int b) {
+        double da = a;
+        double db = b;
+        return (int) super.somar(da, db);
+    }
+
+    @Override
+    public int sub(int a, int b) {
+        double da = a;
+        double db = b;
+        return (int) super.subtrair(da, db);
+    }
+
+    @Override
+    public int mult(int a, int b) {
+        double da = a;
+        double db = b;
+        return (int) super.multiplicar(da, db);
+    }
+
+    @Override
+    public int div(int a, int b) {
+        double da = a;
+        double db = b;
+        return (int) super.dividir(da, db);
+    }
+}
